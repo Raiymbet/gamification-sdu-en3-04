04b5971e1124c6007185540011adc1f3667b17b8
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-02-03 11:39:29 +0000

    Input example folder "project". Use Bootstrap Framework
    Input new file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/stylish-portfolio.css" rel="stylesheet" media="screen">
+    <!-- Add custom CSS here -->
+</head>
+<body>
+<script src="js/jquery-1.10.2.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
