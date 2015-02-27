525f777970c15bcf0f26d7a5708f6a2b4f0749ff
ferdausiuly.aibek@ce.sdu.edu.kz <ferdausiuly.aibek@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-02-27 11:04:36 +0000

    ffff

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php
+
+include ("bd.php");
+
+
+
+$query = mysql_query("SELECT * FROM questions");
+$qqq = mysql_fetch_array($query);
+
+
+
+?>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+
+</body>
+
+</html>
