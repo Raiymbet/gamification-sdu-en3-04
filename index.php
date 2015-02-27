16cd67b224f9bac4635917adf3a0aae4467ac1cb
ferdausiuly.aibek@ce.sdu.edu.kz <ferdausiuly.aibek@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-02-27 11:06:47 +0000

    ffff

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+<div>
+<form action='new_test.php' method='get'>
+<table>
+<tr>
+<td> Write your name  </td>
+</tr>
+<tr><td><input type='text' name='name'> </td></tr>
+<tr><td><input type='submit'> </td></tr>
+
+</table>
+</form>
+</div>
+
+</body>
+
+</html>
