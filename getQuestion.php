8d08a37e7c90027baf4456c8493c5e2c0bbf73ae
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-06 16:12:31 +0000

    LAB6. Akzhol Bakytzhan. I was late, because I have no time for make lab.

--- getQuestion.php
+++ getQuestion.php
@@ -1,10 +1,23 @@
 <?php
-include_once 'connect.php';
-$ID = 1;
-$query = mysqli_query($con, "SELECT * FROM FINDQUESTION WHERE ID=$ID");
-$num_rows = mysqli_num_rows($query);
-while ($row = mysqli_fetch_array($query)) {
-    $array = array('Question'=>$row['Question'],'ANS1' => $row['ANS1'], 'ANS2' => $row['ANS2'], 'ANS3' => $row['ANS3'], 'ANS4' => $row['ANS4'], 'CORRECT' => $row['CORRECT']);
-   echo json_encode($array);
+if (isset($_POST['ID'])) {
+    include_once 'connect.php';
+    $ID = $_POST['ID'];
+    $query = mysqli_query($con, "SELECT * FROM FINDQUESTION WHERE ID=$ID");
+    $num_rows = mysqli_num_rows($query);
+    while ($row = mysqli_fetch_array($query)) {
+        $array = array(
+            'ID' => $row['ID'],
+            'Question' => $row['Question'],
+            'ANS1' => $row['ANS1'],
+            'ANS1_ID'=>$row['ANS1_ID'],
+            'ANS2_ID'=>$row['ANS2_ID'],
+            'ANS3_ID'=>$row['ANS3_ID'],
+            'ANS4_ID'=>$row['ANS4_ID'],
+            'ANS2' => $row['ANS2'],
+            'ANS3' => $row['ANS3'],
+            'ANS4' => $row['ANS4'],
+            'CORRECT' => $row['CORRECT']);
+        echo json_encode($array);
+    }
 }
 ?>
