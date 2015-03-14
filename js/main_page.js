9752e397d440a8b823b0e8c2170af468a282fe0b
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-14 06:41:55 +0000

    Make ArchDesign.jpg

---
+++ js/main_page.js
@@ -0,0 +1,30 @@
+var current = "top";
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+
+            $("#" + current + 1 + "").attr("class", "");
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            var menu = this.hash.slice(1);
+            $("#" + menu + 1).attr("class", "checked");
+            current = menu;
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    function initialize() {
+        var mapProp = {
+            center: new google.maps.LatLng(43.2083297, 76.6689786),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
