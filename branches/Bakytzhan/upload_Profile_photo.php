298d9c6156e4b24a8200906d4dcb0db450796a48
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-20 20:20:19 +0000

    Добавиль проверку соединение к БД Добавиль загрузка фото Добавль функцию для проверки пользователя

--- branches/Bakytzhan/upload_Profile_photo.php
+++ branches/Bakytzhan/upload_Profile_photo.php
@@ -5,6 +5,17 @@
  * 2. Регистрация в базе
  * 3.
  */
+$id = 0;
+//Перед отправкой, надо проверить пользователя.
+//Совет: Не верь пользователю, не верь его слову и его cookie!Надо каждый действие проверять
+//Проверка пользователя
+include_once 'utils.php';
+include_once 'connect.php';
+if (check_user($con)) {
+    $id = $_COOKIE['id'];
+} else {
+    header("auth.html?c=1");
+}
 $validextensions = array("jpeg", "jpg", "png");
 $temporary = explode(".", $_FILES["file"]["name"]);
 $file_extension = end($temporary);
@@ -14,7 +25,6 @@ if ((($_FILES["file"]["type"] == "image/png")
     && ($_FILES["file"]["size"] < 2000000)//approx. 2mb files can be uploaded
     && in_array($file_extension, $validextensions)
 ) {
-    echo "<span>Your File Uploaded Succesfully...!!</span><br/>";
     $filename = $_FILES["file"]["name"];
     $type = $_FILES["file"]["type"];
     $size = ($_FILES["file"]["size"] / 1024);
@@ -26,8 +36,18 @@ if ((($_FILES["file"]["type"] == "image/png")
         move_uploaded_file($_FILES["file"]["tmp_name"],
             "upload/" . $_FILES["file"]["name"]);
     }
+    $result = mysqli_query($con, "UPDATE tb_student SET photo_url='$filename' WHERE id=$id") or die("2");
+    if (mysqli_num_rows($result) > 0) {
+        //Комманда 2F:Что-то не так!
+        mysqli_close($con);
+        return 0;
+    } else {
+        mysqli_close($con);
+        return 2;
+    }
 } else {
     echo "<span>***Invalid file Type or file Size Exceeded***<span>";
 }
+mysqli_close($con);
 //PHP Image Uploading Code
 ?>
