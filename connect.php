58795402b2e787f3bf0503e52ed18690b0c98f2b
alymkhan.dossymbetov@ce.sdu.edu.kz <alymkhan.dossymbetov@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-21 10:07:29 +0000

    Жаңа орын дайындап откан

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$url = "localhost";
+$user = "root";
+$password = "";
+$DB = "db_gamification";
+$con = mysqli_connect($url, $user, $password, $DB) or die("Connection error");
+if (!$con) {
+    $error = mysqli_connect_error();
+    $errno = mysqli_connect_errno();
+    print "$errno: $error\n";
+    exit();
+}
+
+mysqli_query($con, 'set names utf8');
+mysqli_query($con, 'set character_set_server=utf8');
+header("Content-Type: text/html; charset=utf-8");
+?>
