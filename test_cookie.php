b6cfc541e437d1791476b4c18fdc6b0fa41c1512
raiymbet.tukpetov@ce.sdu.edu.kz <raiymbet.tukpetov@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-21 17:35:20 +0000

    FIx DB Make registration form Make authorization form

---
+++ test_cookie.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raiymbet
+ * Date: 21.03.2015
+ * Time: 21:59
+ */
+echo $_COOKIE['id'],
+$_COOKIE['name'],
+$_COOKIE['surname'] ,
+$_COOKIE['birthday'],
+$_COOKIE['group'],
+$_COOKIE['telephone'],
+$_COOKIE['gender'],
+$_COOKIE['photo_url'],
+$_COOKIE['time'];
