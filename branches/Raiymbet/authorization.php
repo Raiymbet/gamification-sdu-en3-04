19f9d9cf59f27996c7cbac7ceaeb28595398647c
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-03-23 08:42:11 +0000

    Training socket High security for authorization

--- branches/Raiymbet/authorization.php
+++ branches/Raiymbet/authorization.php
@@ -11,12 +11,38 @@
 include_once 'connect.php';
 
 if (isset($_POST['submit'])) {
+    if (!isset($_POST['e_mail']) || empty($_POST['e_mail'])) {
+        exit("'e_mail' not found");
+    }
+    if (!isset($_POST['password']) || empty($_POST['password'])) {
+        exit("'password' not found");
+    }
+    if (!isset($_POST['remember_checkbox']) || empty($_POST['remember_checkbox'])) {
+        exit("'remember_checkbox' not found");
+    }
+
     $e_mail = $_POST['e_mail'];
     $password = md5(md5($_POST['password']));
     $remember_checkbox = $_POST['remember_checkbox'];
-    $query = mysqli_query($con, "SELECT * FROM tb_student WHERE email='$e_mail' and password='$password'");
-    if ($query and mysqli_num_rows($query) > 0) {
-        $row = mysqli_fetch_assoc($query);
+
+    if ($stmt = $con->prepare("SELECT * FROM tb_student WHERE email= ?  and password= ? ")
+    ) {
+
+        /* Bind parameters
+           s - string, b - blob, i - int, etc */
+        $stmt->bind_param("ss", $e_mail, $password);
+
+        /* Execute it */
+        $stmt->execute();
+
+        /* Bind results */
+        $res = $stmt->bind_result($result);
+
+        /* Fetch the value */
+        $stmt->fetch();
+
+        $res = $stmt->get_result();
+        $row = $res->fetch_assoc();
         $time = time() + 3600 * 24;
         setcookie("id", $row['id'], $time);
         setcookie("name", $row['name'], $time);
@@ -27,18 +53,23 @@ if (isset($_POST['submit'])) {
         setcookie("gender", $row['gender'], $time);
         setcookie("photo_url", $row['photo_url'], $time);
         setcookie("time", $time, $time);
-        echo "Авторизация прошла успешно! Ваши данные:\n";
-        echo $_COOKIE['id'],"\n",
-        $_COOKIE['name'],"\n",
-        $_COOKIE['surname'] ,"\n",
-        $_COOKIE['birthday'],"\n",
-        $_COOKIE['group'],"\n",
-        $_COOKIE['telephone'],"\n",
-        $_COOKIE['gender'],"\n",
-        $_COOKIE['photo_url'],"\n",
+        echo "<span>Авторизация прошла успешно! Ваши данные:</span><br>";
+        echo $_COOKIE['id'], "\n",
+        $_COOKIE['name'], "\n",
+        $_COOKIE['surname'], "\n",
+        $_COOKIE['birthday'], "\n",
+        $_COOKIE['group'], "\n",
+        $_COOKIE['telephone'], "\n",
+        $_COOKIE['gender'], "\n",
+        $_COOKIE['photo_url'], "\n",
         $_COOKIE['time'];
+        /* Close statement */
+        $stmt->close();
     } else {
-        echo "Неправильный логин или пароль!";
+        echo "<span>Неправильный логин или пароль!</span>";
     }
+
+    /* Close connection */
+    myslqi_close($con);
 }
 ?>
