da4ed966245a3b975bbe87d416677d2eb6a5945e
akzhol.b93@gmail.com <akzhol.b93@gmail.com@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-04-12 18:41:11 +0000

    Parts 1/2 student.php

--- branches/Bakytzhan/student.php
+++ branches/Bakytzhan/student.php
@@ -9,73 +9,73 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link href="../../project/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="../../project/css/stylish-portfolio.css" rel="stylesheet" media="screen">
-    <link href="css/mystyle.css" rel="stylesheet" media="screen">
-</head>
-<style>
-    body {
-        padding-top: 70px;
-    }
-</style>
-<body>
-    <?php
-    include_once 'utils.php';
-    include_once 'connect.php';
+    <link href="css/mystyle.css" rel="stylesheet" media="screen"></head>
+    <style>
+        body {
+            padding-top: 70px;
+        }
+    </style>
+    <body>
+        <?php
+        include_once 'utils.php';
+        include_once 'connect.php';
 //Для пробный проверки достаточные эти данные
-    setcookie("id", "1", time() + 3600);
-    setcookie("name", "Raiymbet", time() + 3600);
-    setcookie("email", "tukpetov@bk.ru", time() + 3600);
-    setcookie("photo_url", "person_1.png", time() + 3600);
-    if (check_user($con) == True) {
-        printf("<script>console.log('Пользователь найден ... OK')</script>
-            ");
-    } else {
-        header("Location: main_page.html");
-    }
-    require_once 'nav.php';
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-2 small" style="position:fixed">
-                <div class="list-group">
-                    <a role="link" href="#" name="Home" class="list-group-item active">Главная</a>
-                    <a role="link" href="#" name="Profile" class="list-group-item">Награды</a>
-                    <a role="link" href="#" name="Messages" class="list-group-item">Рейтинг</a>
-                    <a role="link" class="list-group-item">...</a>
+        setcookie("id", "1", time() + 3600);
+        setcookie("name", "Raiymbet", time() + 3600);
+        setcookie("email", "tukpetov@bk.ru", time() + 3600);
+        setcookie("photo_url", "person_1.png", time() + 3600);
+        if (check_user($con) == True) {
+            printf("<script>console.log('Пользователь найден ... OK')</script>
+                ");
+        } else {
+            header("Location: main_page.html");
+        }
+        require_once 'nav.php';
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-2 small" style="position:fixed">
+                    <div class="list-group">
+                        <a role="link" href="#" name="Home" class="list-group-item active">Главная</a>
+                        <a role="link" href="#" name="Profile" class="list-group-item">Награды</a>
+                        <a role="link" href="#" name="Messages" class="list-group-item">Рейтинг</a>
+                        <a role="link" class="list-group-item">...</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-8 small col-offset-2" id="main-frame">
-                <div class="well" style="height:660px">
-                    <?php
-                    $query=mysqli_query($con,"SELECT A.id as id,A.title as title,A.id_teacher as teacher,A.time_limit as time_limit,A.description as description,COUNT(B.id) as tQuestion FROM `tb_tournaments` A,tb_questions B WHERE B.id_tournament=A.id and A.public=1 and A.id_groups=1") or die(mysqli_errr($con));
+                <div class="col-8 small col-offset-2" id="main-frame">
+                    <div class="well" style="height:660px">
+                        <?php
+                        $query = mysqli_query($con, "SELECT A.id as id,A.title as title,A.id_teacher as teacher,A.time_limit as time_limit,A.description as description,COUNT(B.id) as tQuestion FROM `tb_tournaments` A,tb_questions B WHERE B.id_tournament=A.id and A.public=1 and A.id_groups=1 GROUP BY id,title,teacher,time_limit,description") or die(mysqli_errr($con));
 
-                    if($query && mysqli_num_rows($query)>
-                        0){
-                        while($row=mysqli_fetch_array($query)){
-                            $hours = floor($row['time_limit'] / 3600);
-                            $mins = floor(($row['time_limit'] - ($hours*3600)) / 60);
-                            $secs = floor($row['time_limit'] % 60);
-                            printf('
-                                <div class="panel panel-info">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">%s</h3>
-                                    </div>
-                                    <div class="panel-body">
-                                        <div class="col-2">
-                                            <img class="img-circle" src="img/sc1.png" style="width:50px;height: 50px"></div>
-                                            <div class="col-8">
-                                                <p>Время: %s:%s</p>
-                                                <p>500 Очко</p>
-                                                <p>%s вопросов</p>
-                                            </div>
-                                            <div class="col-1">
-                                                <a href="question.php?id=%s" class="btn btn-danger">Play</a>
+                        if($query && mysqli_num_rows($query)>
+                            0){
+                            while($row=mysqli_fetch_array($query)){
+                                $hours = floor($row['time_limit'] / 3600);
+                                $mins = floor(($row['time_limit'] - ($hours*3600)) / 60);
+                                $secs = floor($row['time_limit'] % 60);
+                                printf('
+                                    <div class="panel panel-info">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">%s</h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="col-2">
+                                                <img class="img-circle" src="img/sc1.png" style="width:50px;height: 50px"></div>
+                                                <div class="col-8">
+                                                    <p>Время: %s:%s</p>
+                                                    <p>500 Очко</p>
+                                                    <p>%s вопросов</p>
+                                                </div>
+                                                <div class="col-1">
+                                                    <a href="question.php?id=%s" class="btn btn-danger">Play</a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    ',$row['title'],($mins>9)?$mins:'0'.$mins,($secs>9)?$secs:'0'.$secs,$row['tQuestion'],$row['id']);}
+                                        ',$row['title'],($mins>9)?$mins:'0'.$mins,($secs>9)?$secs:'0'.$secs,$row['tQuestion'],$row['id']);}
 }
 ?>
-</div></div></div>
+</div>
+</div>
 <!-- Nagrada frame -->
 <div class="col-8 small col-offset-2" id="nagrada-frame" style="display: none">
     <div class="well">
@@ -176,48 +176,57 @@
                         </div>
                     </div>
                     <!-- Надо укоротить код -->
-                    <div class="panel-body">
+                    <div class="panel-body" style="min-height:560px">
                         <div class="raiting_item">
                             <?php
-                            $query=mysqli_query($con,"SELECT 
+                            $query = mysqli_query($con, "SELECT
                                 A.id as id,
-                                B.id as id_student, 
+                                B.id as id_student,
                                 B.name as name,
                                 B.surname as surname,
                                 B.photo_url as photo_url,
-                                A.score as score,
+                                A.score as score, 
                                 SUM(A.score) as total_score,
                                 C.id_groups as id_groups,
-                                D.title as GROUP_NAME 
+                                D.title as group_name 
                                 FROM tb_student_result A,tb_student B,tb_group_students C,tb_groups D 
-                                where A.id_student=B.id and A.id_student=C.id_student and C.id_groups=D.id 
-                                ORDER BY score LIMIT 10");
-                            if($query && mysqli_num_rows($query)){
-                                while($row=mysqli_fetch_array($query)){
-                                }
-                            }
-                            ?>
-                        </div></div></div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="col-1">
-                                    <h2>1</h2>
-                                </div>
-                                <div class="col-1">
-                                    <img src="img/person.png"></div>
-                                    <div class="col-6">
-                                        <h4>Username</h4>
-                                        <h4>Group</h4>
-                                    </div>
-                                    <div class="col-2">
-                                        <h4 class="gold_text">1234567</h4>
-                                    </div>
-                                </div>
-                            </div></div>
-                        </div>
-                        <script src="../../project/js/jquery-1.10.2.js"></script>
-                        <script src="../../project/js/bootstrap.js"></script>
-                        <script type="text/javascript" src="../../project/js/flotr2.min.js"></script>
-                        <script type="text/javascript" src="js/student_js.js"></script>
-                    </body>
-                    </html>
+                                where A.id_student=B.id and A.id_student=C.id_student and C.id_groups=D.id and A.score>200
+                                GROUP BY id,id_student,name,surname,photo_url,score,id_groups,group_name ORDER BY total_score,id_student") or die(mysqli_error($con));
+                            $i = 1;
+                            if (!$query || mysqli_num_rows($query) >
+                                0) {
+                                while ($row = mysqli_fetch_array($query)) {
+                                    printf('
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="col-1">
+                                                    <h2>%s</h2>
+                                                </div>
+                                                <div class="col-1">
+                                                    <img src="img/%s"></div>
+                                                    <div class="col-6">
+                                                        <h4>
+                                                            <a href="../ALimkhan/profile.php?id=%s">%s</a>
+                                                        </h4>
+                                                        <h4>%s</h4>
+                                                    </div>
+                                                    <div class="col-2">
+                                                        <h4 class="gold_text">%s</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            ', $i++, $row['photo_url'], $row['id_student'], $row['surname'] . ' ' . $row['name'], $row['group_name'], $row['total_score']);
+}
+}
+?>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+<script src="../../project/js/jquery-1.10.2.js"></script>
+<script src="../../project/js/bootstrap.js"></script>
+<script type="text/javascript" src="../../project/js/flotr2.min.js"></script>
+<script type="text/javascript" src="js/student_js.js"></script>
+</html>
