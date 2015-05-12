350e0621f1b1df289ef6008b9eb717fcadf33c6b
raiymbet.tukpetov@ce.sdu.edu.kz <raiymbet.tukpetov@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-05-12 20:04:02 +0000

    Save tournament

---
+++ save_tournament.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raiymbet
+ * Date: 02.05.2015
+ * Time: 22:36
+ */
+//
+include_once 'connect.php';
+/*Table tournaments: id, title, id_groups, id_teacher, datetime_added, status, when_opened, when_closed, public, description
+Table questions: id, id_tournament, question, type_question, question_point, time_limit, level_question
+Table variants: id, id_question, correct, text
+*/
+$tournament_info = '';
+$title = $description = $when_opened = $when_closed = $public = $id_groups = $id_teacher = $status = '';
+$datetime_added = date("Y-m-d h:i:s");
+
+echo $datetime_added;
+
+if(isset($_POST['tournament_info'])){
+    $tournament_info = json_decode($_POST['tournament_info'],true);
+    $title = $_POST['tournament_info']['about_tournament']['name'];
+    echo $tournament_info;
+
+    mysqli_query($con, "INSERT INTO tb_tournaments(title, id_groups, id_teacher, datetime_added, time_limit, status, when_opened, when_closed, public, description)
+                        VALUES ($title,)")
+    or die(mysqli_error($con));
+
+    mysqli_query($con, "INSERT INTO tb_questions(id_tournament, question, type_question, question_point, time_limit, level_question)
+                        VALUES ()")
+    or die(mysqli_error($con));
+}
+?>
