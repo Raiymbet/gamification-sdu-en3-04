7b26e4201bbe63db25ecad0de927b65361c225c5
raiymbet.tukpetov@ce.sdu.edu.kz <raiymbet.tukpetov@ce.sdu.edu.kz@18c57f49-bee1-94a9-bf5f-214f6f328989>
2015-05-14 15:28:55 +0000

    Save tournament

--- branches/Raiymbet/save_tournament.php
+++ branches/Raiymbet/save_tournament.php
@@ -12,39 +12,75 @@ Table questions: id, id_tournament, question, type_question, question_point, tim
 Table variants: id, id_question, correct, text
 */
 $tournament_info = '';
-$title = $description = $when_opened = $when_closed = $public = $id_groups = $id_teacher = $status = $datetime_added = '';
+$title = $description = $when_opened = $when_closed = $public = $id_group = $id_teacher = $status = $datetime_added = '';
 
 $id_tournament = $question = $type_question = $question_point = $time_limit = $level_question = '';
 
-if(isset($_POST['dannie'])){
-    $php_json=$_POST['dannie'];
-    $json_array=json_decode($php_json,JSON_UNESCAPED_UNICODE);
+$id_question = $correct = $text = '';
 
+if (isset($_POST['dannie'])) {
+    $php_json = $_POST['dannie'];
+    $json_array = json_decode($php_json, JSON_UNESCAPED_UNICODE);
+
+    //Данные для tb_tournaments
     $title = $json_array['about_tournament']['name'];
     $description = $json_array['about_tournament']['description'];
-    $datetime_added = date("Y-m-d h:i:s");
+    $status = $json_array['about_tournament']['status'];
+    $datetime_added = $json_array['about_tournament']['current_date'];
     $when_opened = $json_array['about_tournament']['opened'];
     $when_closed = $json_array['about_tournament']['closed'];
     $public = $json_array['about_tournament']['public'];
-
-    for($i=0;$i<count($json_array['questions']);$i++){
-        //$id_tournament =
+    $id_group = $json_array['about_tournament']['id_groups'];
+    $id_teacher = $json_array['about_tournament']['id_teacher'];
+    echo "INSERT INTO tb_tournaments(title, id_groups, id_teacher, datetime_added, status, when_opened, when_closed, public, description)
+                        VALUES ('$title', '$id_group', '$id_teacher', '$datetime_added', '$status', '$when_opened', '$when_closed', '$public', '$description')";
+    mysqli_query($con, "INSERT INTO tb_tournaments(title, id_groups, id_teacher, datetime_added, status, when_opened, when_closed, public, description)
+                        VALUES ('$title', '$id_group', '$id_teacher', '$datetime_added', '$status', '$when_opened', '$when_closed', '$public', '$description')")
+                        or die(mysqli_error($con));
+    for ($i = 0; $i < count($json_array['questions']); $i++) {
+        echo $i;
+        //Данные для tb_questions нет id_tournament
         $question = $json_array['questions'][$i]['question'];
         $type_question = $json_array['questions'][$i]['game_type'];
         $question_point = $json_array['questions'][$i]['point'];
         $time_limit = $json_array['questions'][$i]['time_limit'];
         $level_question = $json_array['questions'][$i]['level'];
 
+        $rt_id = mysqli_query($con, "SELECT id as id FROM tb_tournaments WHERE title='$title'");
+        $row = mysqli_fetch_array($rt_id);
+        $id_sa=$row['id'];
         mysqli_query($con, "INSERT INTO tb_questions(id_tournament, question, type_question, question_point, time_limit, level_question)
-                        VALUES ((SELECT MAX(id) FROM tb_tournaments))")
-        or die(mysqli_error($con));
+                    VALUES ('$id_sa', '$question', '$type_question', '$question_point', '$time_limit', '$level_question')")
+                    or die(mysqli_error($con));
+        //Данные для tb_variants
+        $correct = 0;
+        if($type_question=='Simple' || $type_question=='True-false'){
+            $correct_answer_index = $json_array['questions'][$i]['correct_answer_id'];
+            for ($j = 0; $j < count($json_array['questions'][$i]['answers']); $j++) {
+                if($j==$correct_answer_index){
+                    $correct = 1;
+                }else{
+                    $correct = 0;
+                }
+                $text = $json_array['questions'][$i]['answers'][$j];
+                $rq_id = mysqli_query($con, "SELECT id FROM tb_questions WHERE id_tournament='$id_sa' and question='$question'");
+                $row_q = mysqli_fetch_array($rq_id);
+                $id_sa_q=$row_q['id'];
+                echo $id_sa_q;
+                mysqli_query($con, "INSERT INTO tb_variants(id_question, correct, text)
+                        VALUES ('$id_sa_q','$correct', '$text')")
+                or die(mysqli_error($con));
+                //echo 'ASDASDASD2222';
+            }
+        }else{
+            $correct = $json_array['questions'][$i]['correct'];
+            for ($j = 0; $j < count($json_array['questions']['answers']); $j++) {
+                $text = $json_array['questions'][$i]['answers'][$j];
+                mysqli_query($con, "INSERT INTO tb_variants(id_question, correct, text)
+                        VALUES ((SELECT id FROM tb_questions WHERE question='$question'),'$correct', '$text')")
+                or die(mysqli_error($con));
+            }
+        }
     }
-    mysqli_query($con, "INSERT INTO tb_tournaments(title, id_groups, id_teacher, datetime_added, time_limit, status, when_opened, when_closed, public, description)
-                        VALUES ($title,)")
-    or die(mysqli_error($con));
-
-    mysqli_query($con, "INSERT INTO tb_variants(id_question, correct, text)
-                        VALUES ()")
-    or die(mysqli_error($con));
 }
 ?>
